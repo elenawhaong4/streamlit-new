bcc3e4393d92234a18c556fb7624512c2cefa603
eunwha27 <eunwha27@bergen.org>
2024-12-29 14:22:40 -0500

    added dr. seuss prompt

--- vb_chatgpt_app1.py
+++ vb_chatgpt_app1.py
@@ -9,6 +9,18 @@ st.title("Eunseo ChatGPT App")
 # with st.chat_message("assistant"):
 #     st.write("Hello human")
 
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo", 
+                                 temperature=0, 
+                                 max_tokens=500):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+        max_tokens=max_tokens, # the maximum number of tokens the model can ouptut 
+    )
+    return response.choices[0].message["content"]
+
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
@@ -31,12 +43,17 @@ if prompt:
     st.session_state.messages.append({"role": "user", "content":prompt})
     
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
+        messages = [
+                        {'role':'system', 
+                        'content':"""You are an assistant who responds in the style of Dr Seuss."""},
+                    ]
+        messages +=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=messages,
             stream=True,
         )
         response = st.write_stream(stream)
